be72fc98a19e892861f22fe9a1ab755c793d3d3b
ammar-rasheed@hotmail.com <ammar-rasheed@hotmail.com>
2023-07-26 18:01:16 +0300

    Your commit message here

--- index.js
+++ index.js
@@ -19,23 +19,40 @@ const publicDirectoryPath = path.join(__dirname, 'public');
 app.use(express.static(publicDirectoryPath));
 
 const capitalMap = new Map([
-  [32101201, 500],
-  [31876293, 1000],
-  [31814867, 1000],
-  [32103676, 2000],
-  [32178454, 1000],
-  [32427107, 4000],
-  [32427154, 2000],
-  [32427159, 145],
-  [32428979, 2000],
-  [32433201, 152],
-  [32208556, 500],
-  [32268993, 500],
-  [32423648, 3000],
-  [32244363, 3000],
-  [32244371, 2000],
-  [32423630, 427],
-  [32435532, 2000],
+  [32101201, { title: 'G9', capital: 500, bots: ['11454883', '11454884'] }],
+
+  [31876293, { title: 'G11', capital: 1000, bots: ['botId1', 'botId2'] }],
+
+  [31814867, { title: 'G13', capital: 1000, bots: ['botId1', 'botId2'] }],
+
+  [32103676, {title: 'G14', capital: 2000, bots: ['botId1', 'botId2'] }],    
+
+  [32178454, { title: 'G24',capital: 1000, bots: ['botId1', 'botId2'] }],    
+
+  [32427107 ,{ title: 'G122',capital: 4000, bots: ['botId1', 'botId2'] }],    
+
+  [32427154,{ title: 'G124', capital: 2000, bots: ['botId1', 'botId2'] }],    
+
+  [32427159, { title: 'G125',capital: 145, bots: ['botId1', 'botId2'] }],    
+
+  [32428979, { title: 'G66',capital: 2000, bots: ['botId1', 'botId2'] }],    
+
+  [32433201 ,{ title: 'G67',capital: 152, bots: ['botId1', 'botId2'] }],    
+
+  [32208556 ,{ title: 'G27',capital: 500, bots: ['botId1', 'botId2'] }],    
+
+  [32268993 ,{title:  'G112',capital: 500, bots: ['botId1', 'botId2'] }],    
+
+  [32423648, { title: '103',capital: 3000, bots: ['botId1', 'botId2'] }],    
+
+  [32244363, { title: 'G117',capital: 3000, bots: ['botId1', 'botId2'] }],    
+
+  [32244371, { title: 'G118',capital: 2000, bots: ['botId1', 'botId2'] }],    
+
+  [32423630 ,{title: 'G98',capital: 427, bots: ['botId1', 'botId2'] }],    
+
+  [ 32435532,{title: 'G72',capital: 2000, bots: ['botId1', 'botId2'] }],    
+
 ]);
 
 app.get('/data', async (req, res) => {
@@ -49,48 +66,90 @@ app.get('/data', async (req, res) => {
       32423630, 32435532,
     ];
 
-    const api1Results = await Promise.all(api1Ids.map(async (id) => {
-      const account = await api1.accountLoadBalances(id);
-      // Process the account data and return the required object format
-      return {
-        id,
-        name: account?.name,
-        balance: Math.floor(account?.primary_display_currency_amount?.amount) || 0,
-        capital: capitalMap.get(id) || 0,
-      };
-    }));
+    // ... (Your existing code)
 
-    const api2Results = await Promise.all(api2Ids.map(async (id) => {
-      const account = await api2.accountLoadBalances(id);
-      // Process the account data and return the required object format
-      return {
-        id,
-        name: account?.name,
-        balance: Math.floor(account?.primary_display_currency_amount?.amount) || 0,
-        capital: capitalMap.get(id) || 0,
-      };
-    }));
+const api1Results = await Promise.all(api1Ids.map(async (id) => {
+  const account = await api1.accountLoadBalances(id);
+  const capitalInfo = capitalMap.get(id);
+  const balance = Math.floor(account?.primary_display_currency_amount?.amount) || 0;
+  const capital = capitalInfo?.capital || 0;
+  const percentage = ((balance - capital) / capital * 100).toFixed(1);
+  return {
+    title: capitalInfo?.title || '',
+    id,
+    name: account?.name,
+    balance,
+    capital,
+    bots: capitalInfo?.bots || [],
+    percentage,
+  };
+}));
+
+const api2Results = await Promise.all(api2Ids.map(async (id) => {
+  const account = await api2.accountLoadBalances(id);
+  const capitalInfo = capitalMap.get(id);
+  const balance = Math.floor(account?.primary_display_currency_amount?.amount) || 0;
+  const capital = capitalInfo?.capital || 0;
+  const percentage = ((balance - capital) / capital * 100).toFixed(1);
+  return {
+    title: capitalInfo?.title || '',
+    id,
+    name: account?.name,
+    balance,
+    capital,
+    bots: capitalInfo?.bots || [],
+    percentage,
+  };
+}));
+
+// ... (Your existing code)
+
 
     const data = {
       api1: api1Results.sort((account1, account2) => account2.balance - account1.balance),
       api2: api2Results.sort((account1, account2) => account2.balance - account1.balance),
     };
 
-    data.api1.forEach((result) => {
-      result.percentage = ((result.balance - result.capital) / result.capital * 100).toFixed(1);
-      result.percentageColor = result.percentage < 0 ? 'red' : 'green';
-    });
-
-    data.api2.forEach((result) => {
-      result.percentage = ((result.balance - result.capital) / result.capital * 100).toFixed(1);
-      result.percentageColor = result.percentage < 0 ? 'red' : 'green';
-    });
-
+   
     res.json(data);
   } catch (error) {
     res.status(500).json({ error: 'Error fetching balances from APIs' });
   }
 });
+
+const stopAllBots = async (accountId) => {
+  try {
+    const api1Ids = [32101201, 31876293, 32103676, 32178454, 32427154, 32427107, 32428979, 32433201, 32427159, 31814867];
+    const api2Ids = [32208556, 32268993, 32423648, 32244363, 32244371, 32423630, 32435532];
+
+    if (api1Ids.includes(accountId)) {
+      // Implement the logic to stop all bots for the given account ID using API 1
+      // ... (Your logic to stop bots using API 1 goes here)
+      return { success: true, message: 'All bots stopped successfully for account ID: ' + accountId };
+    } else if (api2Ids.includes(accountId)) {
+      // Implement the logic to stop all bots for the given account ID using API 2
+      // ... (Your logic to stop bots using API 2 goes here)
+      return { success: true, message: 'All bots stopped successfully for account ID: ' + accountId };
+    } else {
+      throw new Error('Invalid account ID');
+    }
+  } catch (error) {
+    console.error('Error stopping bots:', error);
+    throw new Error('Error stopping bots for account ID: ' + accountId);
+  }
+};
+
+app.post('/stop-all-bots', async (req, res) => {
+  try {
+    const { accountId } = req.body;
+    const result = await stopAllBots(accountId);
+    res.json(result);
+  } catch (error) {
+    console.error('Error stopping bots:', error);
+    res.status(500).json({ error: 'Error stopping bots' });
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- public/client.js
+++ public/client.js
@@ -1,39 +1,39 @@
 const createTableRow = (account) => {
-    const tableRow = document.createElement('tr');
-    tableRow.innerHTML = `
+  const tableRow = document.createElement('tr');
+  tableRow.innerHTML = `
+      <td>${account.title}</td>
+      <td>${account.id}</td>
       <td>${account.name}</td>
       <td>$${account.balance}</td>
-      <td class="${account.percentage >= 0 ? 'positive-percentage' : 'negative-percentage'}">${account.percentage}</td>
-      <td><button onclick="closeAllPositions(${account.id})">Close All Positions</button></td>
-    `;
-    return tableRow;
-  };
-  
-  const renderData = (data) => {
-    const api1Table = document.getElementById('api1-table');
-    const api2Table = document.getElementById('api2-table');
-  
-    data.api1.forEach((account) => {
-      const tableRow = createTableRow(account);
-      api1Table.appendChild(tableRow);
-    });
-  
-    data.api2.forEach((account) => {
-      const tableRow = createTableRow(account);
-      api2Table.appendChild(tableRow);
-    });
-  };
-  
-  fetch('/data')
-    .then((response) => response.json())
-    .then((data) => {
-      renderData(data);
-    })
-    .catch((error) => {
-      console.error('Error fetching data:', error);
-    });
-  
-  function closeAllPositions(accountId) {
-    console.log(`Closing all positions for account with ID: ${accountId}`);
+      <td>$${account.capital}</td>
+      <td>${account.bots.join(', ')}</td>
+      <td>${account.percentage}%</td>
+      <td>
+          <button onclick="closeAllPositions(${account.id})">Close All Positions</button>
+          <button onclick="stopAllBots(${account.id})">Stop Bots</button>
+      </td>
+  `;
+
+  // Apply percentage color based on the percentage value
+  const percentageCell = tableRow.querySelector('td:nth-child(7)');
+  const percentage = parseFloat(account.percentage);
+  if (!isNaN(percentage)) {
+    if (percentage < 0) {
+      percentageCell.style.color = 'red';
+    } else {
+      percentageCell.style.color = 'green';
+    }
   }
-  
+
+  return tableRow;
+};
+
+// Fetch the account data from the server
+fetch('/data')
+  .then((response) => response.json())
+  .then((data) => {
+    // ... Your existing code to handle the received data and create account rows
+  })
+  .catch((error) => {
+    console.error('Error fetching account data:', error);
+  });
